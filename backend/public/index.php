6f62fbb200d23600d62b194c4f160bd6f17bbf33
slahino <73803271+slahino@users.noreply.github.com>
2021-03-23 15:30:34 +0100

    first request all users

--- backend/public/index.php
+++ backend/public/index.php
@@ -4,7 +4,9 @@ require_once __DIR__ . '/../src/vendor/autoload.php' ;
 
 $api_settings = require_once __DIR__ . '/../src/conf/api_settings.php';
 $api_errors = require_once __DIR__ . '/../src/conf/api_errors.php';
-use udalost\backend\controller\CommandeController;
+
+//use udalost\backend\controller\CommandeController;
+use udalost\backend\models\Utilisateur as Utilisateur;
 
 $api_container = new \Slim\Container(array_merge($api_settings, $api_errors));
 
@@ -13,9 +15,8 @@ $app = new \Slim\App($api_container);
 \udalost\backend\connection\DataBaseConnection::startEloquent($api_container->settings['db']);
 
 
-/*$evenement = Carte::where('id', '=', 42)->first();
-echo "\n		Afficher la carte n° 42 et ses commandes.
-	  \nID de la carte : $carte->id \n\n";
-foreach ($liste_commandes as $v) {
-  echo "Commande n° $v->id\nClient : $v->nom_client\nDe la Carte n° $v->carte_id\n\n";
-}*/
+$requete = Utilisateur::select();
+$lignes = $requete->get();
+foreach ($lignes as $v) {
+  echo "<br>Nom du propriétaire : $v->nom";
+}
