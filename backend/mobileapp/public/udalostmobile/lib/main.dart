0eaccb3bd9a4b6ccd16cb29f3500295ae929f0ac
tahathedancer98 <bookingtaha98@gmail.com>
2021-03-26 09:29:46 +0100

    Partie MOBILE OK pour commencé les WIDGETS

--- backend/mobileapp/public/udalostmobile/lib/main.dart
+++ backend/mobileapp/public/udalostmobile/lib/main.dart
@@ -20,9 +20,9 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.pink,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Udalost'),
     );
   }
 }
@@ -94,7 +94,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'You have pushed the button this many times:',
+              'COMING SOON, CLICK TO SPEND TIME:',
             ),
             Text(
               '$_counter',
