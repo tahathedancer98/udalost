ac663f3811709c38befe37d7a51cf7803e11a353
Antonin-Winterstein <60510079+Antonin-Winterstein@users.noreply.github.com>
2021-03-30 15:27:57 +0200

    Modifications CORS

--- backend/webapp/public/index.php
+++ backend/webapp/public/index.php
@@ -19,8 +19,8 @@ $api_container = new \Slim\Container(array_merge($api_settings, $api_errors));
 
 $app = new \Slim\App($api_container);
 
+$app->add(Cors::class.':checkHeaderOrigin');
 
-//Config et Connexion à la BDD
 DataBaseConnection::startEloquent($api_container->settings['db']);
 
 // print '<h1>API WEB</h1>';
@@ -28,41 +28,41 @@ DataBaseConnection::startEloquent($api_container->settings['db']);
 //* Les objets de type requête
 // Utilisateurs
 // Afficher tous les utilisateurs
-$app->get('/utilisateurs[/]', UserController::class . ':users')->setName('utilisateurs')->add(Cors::class.':checkHeaderOrigin')->add(Cors::class.':headersCORS');
+$app->get('/utilisateurs[/]', UserController::class . ':users')->setName('utilisateurs');
 // Afficher un utilisateur en particulier
-$app->get('/utilisateurs/{id}[/]', UserController::class . ':aUser')->setName('utilisateur')->add(Cors::class.':checkHeaderOrigin')->add(Cors::class.':headersCORS');
+$app->get('/utilisateurs/{id}[/]', UserController::class . ':aUser')->setName('utilisateur');
 // Créer un utilisateur
-$app->post('/utilisateurs[/]', UserController::class.':createUser')->setName('creerUtilisateur')->add(Cors::class.':checkHeaderOrigin')->add(Cors::class.':headersCORS');
+$app->post('/utilisateurs[/]', UserController::class.':createUser')->setName('creerUtilisateur');
 // Se connecter à son compte 
-$app->post('/connexion[/]', UserController::class.':loginUser')->setName('connexion')->add(Cors::class.':checkHeaderOrigin')->add(Cors::class.':headersCORS');
+$app->post('/connexion[/]', UserController::class.':loginUser')->setName('connexion');
 // Modifier un utilisateur
-$app->put('/utilisateurs/{id}', UserController::class.':editUser')->setName('modifierUtilisateur')->add(Cors::class.':checkHeaderOrigin')->add(Cors::class.':headersCORS');
+$app->put('/utilisateurs/{id}', UserController::class.':editUser')->setName('modifierUtilisateur');
 // Supprimer un utilisateur
-$app->delete('/utilisateurs/{id}[/]', UserController::class.':deleteUser')->setName('supprimerUtilisateur')->add(Cors::class.':checkHeaderOrigin')->add(Cors::class.':headersCORS');
+$app->delete('/utilisateurs/{id}[/]', UserController::class.':deleteUser')->setName('supprimerUtilisateur');
 
 
 // Événements
 // Afficher tous les événements
-$app->get('/evenements[/]', EventController::class . ':events')->setName('evenements')->add(Cors::class.':checkHeaderOrigin')->add(Cors::class.':headersCORS');
+$app->get('/evenements[/]', EventController::class . ':events')->setName('evenements');
 // Afficher un événement en particulier
-$app->get('/evenements/{id}[/]', EventController::class . ':anEvent')->setName('evenement')->add(Cors::class.':checkHeaderOrigin')->add(Cors::class.':headersCORS');
+$app->get('/evenements/{id}[/]', EventController::class . ':anEvent')->setName('evenement');
 // Créer un événement
-$app->post('/evenements[/]', EventController::class.':addEvent')->setName('creerEvenement')->add(Cors::class.':checkHeaderOrigin')->add(Cors::class.':headersCORS');
+$app->post('/evenements[/]', EventController::class.':addEvent')->setName('creerEvenement');
 // Laisser un commentaire sur l'événement
-$app->post('/evenements/{id}/commentaires[/]', EventController::class.':addComment')->setName('ajouterCommentaire')->add(Cors::class.':checkHeaderOrigin')->add(Cors::class.':headersCORS');
+$app->post('/evenements/{id}/commentaires[/]', EventController::class.':addComment')->setName('ajouterCommentaire');
 // Modifier un événement
-$app->put('/evenements/{id}', EventController::class.':editEvent')->setName('modifierEvenement')->add(Cors::class.':checkHeaderOrigin')->add(Cors::class.':headersCORS');
+$app->put('/evenements/{id}', EventController::class.':editEvent')->setName('modifierEvenement');
 // Rejoindre un événement en tant que participant
-$app->put('/evenements/{id}/rejoindre[/]', EventController::class.':joinEvent')->setName('rejoindreEvenement')->add(Cors::class.':checkHeaderOrigin')->add(Cors::class.':headersCORS');
+$app->put('/evenements/{id}/rejoindre[/]', EventController::class.':joinEvent')->setName('rejoindreEvenement');
 // Supprimer un événement
-$app->delete('/evenements/{id}[/]', EventController::class.':deleteEvent')->setName('supprimerEvenement')->add(Cors::class.':checkHeaderOrigin')->add(Cors::class.':headersCORS');
+$app->delete('/evenements/{id}[/]', EventController::class.':deleteEvent')->setName('supprimerEvenement');
 
 
 // Participants
 // Afficher tous les participants
-$app->get('/participants[/]', ParticipantController::class . ':participants')->setName('participants')->add(Cors::class.':checkHeaderOrigin')->add(Cors::class.':headersCORS');
+$app->get('/participants[/]', ParticipantController::class . ':participants')->setName('participants');
 // Afficher un participant en particulier
-$app->get('/participants/{id}[/]', ParticipantController::class . ':aParticipant')->setName('participant')->add(Cors::class.':checkHeaderOrigin')->add(Cors::class.':headersCORS');
+$app->get('/participants/{id}[/]', ParticipantController::class . ':aParticipant')->setName('participant');
 
 // Déclenche le traitement par le framework de la requête courante et la compare dans l'ordre de chacune des routes
 try {
