da8a25e4b6dfd0346781841617b65fb10f7fc0ab
Antonin-Winterstein <60510079+Antonin-Winterstein@users.noreply.github.com>
2021-04-01 17:33:53 +0200

    Suppression de l'ancien dossier mobile du backend, déplacement du readme

--- backend/mobileapp/public/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
- require_once __DIR__ . '/../src/test.php' ;
- require_once __DIR__ . '/../src/vendor/autoload.php' ;
-
- $app = new \Slim\App();
-
- test();
--- backend/mobileapp/public/udalostmobile/android/app/src/main/kotlin/com/example/udalost/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.udalost
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
